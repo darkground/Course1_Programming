590bbc8155cdc99598840818b2de8647531d3e80
darkground <nailedhammer@yandex.ru>
2022-11-26 15:04:30 +0300

    Task 1 - Matrix generation in circles and snakes

--- cpp3/cpp3.cpp
+++ cpp3/cpp3.cpp
@@ -1,7 +1,8 @@
 ﻿#include <iostream>
 #include <limits> //нужно для numeric_limits
+#include <iomanip> //нужно для setw (красивый вывод)
 
-const int N = 6;
+const int N = 6; //8 or 10
 
 using namespace std;
 
@@ -29,6 +30,91 @@ T readValue() {
     }
 }
 
+/*
+*   Функция для вывода матрицы в терминал
+*   setw используется для задания ширины поля числа (т.е. вывод будет красивым, в виде таблицы)
+*/
+void printMatrix(int (*arr)[N]) {
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < N; j++) {
+            int v = *(*(arr + i) + j);
+            if (v == 0) // Если число равно нулю, то просто вывести точку (для видимости)
+                cout << "   ."; 
+            else
+                cout << setw(4) << v;
+        }
+        cout << endl;
+    }
+}
+
+/*
+*   Функция для установки всех чисел матрицы на ноль
+*/
+void emptyMatrix(int (*arr)[N]) {
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < N; j++) {
+            *(*(arr + i) + j) = 0;
+        }
+    }
+}
+
+/*
+*   Промежуточный этап заполнения матрицы:
+*   Поэтапный вывод полученной матрицы, вывод того, что изменилось
+*/
+void fillStep(int (*arr)[N], int i, int j, int value) {
+    system("cls");
+    *(*(arr + i) + j) = value;
+    printMatrix(arr);
+    cout << "Value at " << "[" << i + 1 << ", " << j + 1 << "] was set to " << value << endl;
+    system("pause");
+}
+
+/*
+*   Получить рандомное число от 1 до N * N
+*/
+int rval() {
+    return rand() % (N*N - 1) + 1;
+}
+
+/*
+*   Задание 1, часть 1
+*   Поэтапно заполнить матрицу кругом случайными числами от 1 до N * N
+*/
+void fillCircled(int (*arr)[N]) {
+    emptyMatrix(arr);
+    int loops = 0;
+    while (loops < N) {
+        for(int i = loops; i < N - loops; i++)
+            fillStep(arr, loops, i, rval());
+        for(int i = loops + 1; i < N - loops; i++)
+            fillStep(arr, i, N - loops - 1, rval());
+        for(int i = N - loops - 2; i >= loops; i--)
+            fillStep(arr, N - loops - 1, i, rval());
+        for(int i = N - loops - 2; i > loops; i--)
+            fillStep(arr, i, loops, rval());
+        
+        loops++;
+    }
+}
+
+/*
+*   Задание 1, часть 2
+*   Поэтапно заполнить матрицу змейкой случайными числами от 1 до N * N
+*/
+void fillSnake(int (*arr)[N]) {
+    emptyMatrix(arr);
+    int column = 0;
+    while (column < N) {
+        for(int i = 0; i < N; i++)
+            fillStep(arr, i, column, rval());
+        column++;
+        for(int i = N - 1; i >= 0; i--)
+            fillStep(arr, i, column, rval());
+        column++;
+    }
+}
+
 int main()
 {
     if (N % 2 != 0) {
@@ -36,22 +122,25 @@ int main()
         return 0;
     }
     srand(time(0));
-    int array[N][N]{};
+    int arr[N][N]{};
     while (true) {
         system("cls");
+        cout << "Matrix (" << N << "x" << N << "):\n";
+        printMatrix(arr);
         cout <<
             "Choose a category from below:\n"
             "0. Exit\n"
-            "1. Fill matrix\n"
-            "2. Switch blocks (A)\n"
-            "3. Switch blocks (B)\n"
-            "4. Switch blocks (C)\n"
-            "5. Switch blocks (D)\n"
-            "6. Sort matrix\n"
-            "7. Append number (+)\n"
-            "8. Substract number (-)\n"
-            "9. Multiply by a number (*)\n"
-            "10. Divide by a number (/)\n\n";
+            "1. Fill matrix (circles)\n"
+            "2. Fill matrix (snake)\n"
+            "3. Switch blocks (A)\n"
+            "4. Switch blocks (B)\n"
+            "5. Switch blocks (C)\n"
+            "6. Switch blocks (D)\n"
+            "7. Sort matrix\n"
+            "8. Append number (+)\n"
+            "9. Substract number (-)\n"
+            "10. Multiply by a number (*)\n"
+            "11. Divide by a number (/)\n\n";
         cout << "Type a number to continue: ";
         int choice = readValue<int>();
         cout << endl;
@@ -59,23 +148,35 @@ int main()
         case 0:
             return 0;
         case 1:
+            fillCircled(arr);
+            system("cls");
+            printMatrix(arr);
+            cout << "Matrix generation complete." << endl;
             break;
         case 2:
-             break;
+            fillSnake(arr);
+            system("cls");
+            printMatrix(arr);
+            cout << "Matrix generation complete." << endl;
+            break;
         case 3:
-             break;
+            break;
         case 4:
-             break;
+            break;
         case 5:
-             break;
+            break;
         case 6:
-             break;
+            break;
         case 7:
-             break;
+            break;
         case 8:
-             break;
+            break;
         case 9:
-             break;
+            break;
+        case 10:
+            break;
+        case 11:
+            break;
         default:
             cout << "\nCategory with number " << choice << " does not exist." << endl;
         }
